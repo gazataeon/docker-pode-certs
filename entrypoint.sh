7b4d4f1b9885f182ecd4ae19176950019287344e
Gaz <gareth@aeon-it.co.uk>
2019-09-24 18:21:04 +0800

    Supports env vars now

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,22 @@ openssl req -new -key docker.key -out docker.csr -subj "/C=UK/ST=Warrington/L=Bi
 openssl x509 -req -days 365 -in docker.csr -signkey docker.key -out docker.crt
 openssl pkcs12 -export -out docker.pfx -inkey docker.key -passout pass:podeRocksSocks -in docker.crt
 
+if [ -z "$podebranch" ]
+then
+echo "No Branch passed, defaulting to develop"
+podebranch="develop"
+fi
+
 cd /usr/Pode/
-git checkout pode-listener
+git checkout $podebranch
 git pull
 cp /usr/web-sockets.ps1 /usr/Pode/examples/web-sockets.ps1
 
-pwsh -c "cd /usr/Pode/examples; ./web-sockets.ps1"
+
+if [ -z "$examplefile" ]
+then
+echo "No examplefile passed, defaulting to web-sockets.ps1"
+examplefile="web-sockets.ps1"
+fi
+
+pwsh -c "cd /usr/Pode/examples; ./$examplefile"
--- web-sockets.ps1
+++ web-sockets.ps1
@@ -6,7 +6,10 @@ Start-PodeServer -Type Pode -Threads 5 {
 
     # listen on HTTPS and HTTP
     #Add-PodeEndpoint -Address * -Port 8080 -Protocol Http
-    Add-PodeEndpoint -Address * -Port 9001 -CertificateFile './certs/docker.pfx' -CertificatePassword 'podeRocksSocks' -Protocol Https
+    if ([string]::IsNullOrEmpty($env:port))
+    {$portNo = 9001}
+    else {$portNo = $env:port}
+    Add-PodeEndpoint -Address * -Port $portNo -CertificateFile './certs/docker.pfx' -CertificatePassword 'podeRocksSocks' -Protocol Https
 
     # log requests to the terminal
     New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
